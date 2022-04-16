bb0855c73a2eacd231ebb1c0eebb3e1b691d8808
Wenceslau Neto <wbeneto@ymail.com>
2022-04-16 16:22:49 -0300

    --Updates..

---
+++ src/test/java/com/ead/serviceregister/DecoderServiceRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ead.serviceregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecoderServiceRegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
